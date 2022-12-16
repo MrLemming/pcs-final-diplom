07f5e0f48ff7a4be7ebacbbce11a1e0eb1f88e55
Aleksey Esin <aesin87@mail.ru>
2022-12-16 12:15:12 +0300

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,38 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
+
+    public static final int PORT = 8989;
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+            System.out.println("Запуск сервера " + PORT + "...");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    String word = in.readLine();
+                    if (word.length() == 0) {
+                        throw new IllegalStateException("Не корректный ввод");
+                    }
+                    Gson gson = new Gson();
+                    List<PageEntry> result = engine.search(word);
+                    out.println(gson.toJson(result));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
